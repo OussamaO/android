f19a0ef66277ef27e98f6f6d2bfc1954d524ec2a
Oussama <oouali@roc-dev.com>
2016-03-08 12:54:58 +0100

    tictactoe

---
+++ TicTacToe/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace TicTacToe
+{
+	[Activity (Label = "TicTacToe", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+		}
+
+		public void setImg(ImageView View)
+		{
+			ImageView imageView = FindViewById<ImageView>(Resource.Id.imageView0);
+		//	ImageView.SetImageResource (Resource.Drawable.rondje);
+		}
+	}
+}
